6abe8c723790afa513087b5bf6105320b28d3c03
Raquel Carvalho <60790005+raquelboriero@users.noreply.github.com>
2024-08-19 23:12:05 +0100

    exercise6

--- cypress/e2e/TAT-CSC.cy.js
+++ cypress/e2e/TAT-CSC.cy.js
@@ -9,15 +9,16 @@ describe('TAT Customer Service Center', () => {
     cy.title().should('eq','TAT Customer Service Center')
   })
 
+
+
+  //EXERCISE + EXERCISE 1
   it('fills in the required fields and submits the form', () => {
     
     cy.get('input[id="firstName"]')
     .should('be.visible')
     .type('Raquel')
     .should('have.value', 'Raquel')
-    //.type in the fields: First name, Last name, Email and How ​​can we help you? Any praise or feedback for us?
-    //.click(Send button)
-    //.should the message is visble and has an element with the success class )
+  
     cy.get('input[id="lastName"]')
     .should('be.visible')
     .type('Boriero')
@@ -28,15 +29,90 @@ describe('TAT Customer Service Center', () => {
     .type('raquel@gmail.com')
     .should('have.value', 'raquel@gmail.com')
 
-    cy.get('input[id="open-text-area"]')
-    //.should('be.visible')
-    .type('my first text')
-    .should('have.value', 'my first text')
+    cy.get('textarea[id="open-text-area"]')
+      .should('be.visible')
+      .type('O professor Sebastião Salvino, morador de Patrocínio, no Alto Paranaíba, foi o responsável por registrar mais de 200 espécies de pássaros ao longo de 20 anos.', { delay: 0 }) // sem atraso ao digitar o texto
+      //.type('O professor Sebastião Salvino, morador de Patrocínio, no Alto Paranaíba, foi o responsável por registrar mais de 200 espécies de pássaros ao longo de 20 anos.') 
+      .should('have.value', 'O professor Sebastião Salvino, morador de Patrocínio, no Alto Paranaíba, foi o responsável por registrar mais de 200 espécies de pássaros ao longo de 20 anos.')
 
+    cy.get('button')
+    .should('be.visible')
+    .click()
+
+    cy.get('span[class="success"]')
+    .should('be.visible') 
     
   })
 
+  //EXERCISE 2 
+  it('displays an error message when submitting the form with an email with invalid formatting', () => {
+    
+    cy.get('input[id="firstName"]')
+    .should('be.visible')
+    .type('Raquel')
+    .should('have.value', 'Raquel')
+  
+    cy.get('input[id="lastName"]')
+    .should('be.visible')
+    .type('Boriero')
+    .should('have.value', 'Boriero')
 
+    cy.get('input[id="email"]')
+    .should('be.visible')
+    .type('raquel')
+    .should('have.value', 'raquel')
 
+    cy.get('textarea[id="open-text-area"]')
+      .should('be.visible')
+      .type('My text.') 
+      .should('have.value', 'My text.')
+
+    cy.get('button')
+    .should('be.visible')
+    .click()
+
+    cy.get('span[class="error"]')
+    .should('be.visible')   
+  })
+
+    //EXERCISE 3 
+  it('validate that if a non-numeric value is entered, its value will remain empty', () => {
+    
+    cy.get('input[id="phone"]')
+    .should('be.visible')
+    .type('abc')
+    .should('have.value', '')
+    
+  })
+
+    //EXERCISE 4 
+  it('displays an error message when the phone becomes required but is not filled in before the form submission', () => {
+    
+    cy.get('input[id="firstName"]').should('be.visible').type('Raquel').should('have.value', 'Raquel')
+    cy.get('input[id="lastName"]').should('be.visible').type('Boriero').should('have.value', 'Boriero')
+    cy.get('input[id="email"]').should('be.visible').type('raquel').should('have.value', 'raquel')
+
+    cy.get('input[id="phone"]').invoke('attr', 'required-mark', 'required')
+  
+    cy.get('button').should('be.visible').click()
+    cy.get('span[class="error"]').should('be.visible') 
+  })
+
+    //EXERCISE 5 
+    it('fills and clears the first name, last name, email, and phone fields', () => {
+    
+      cy.get('input[id="firstName"]').should('be.visible').type('Raquel').should('have.value', 'Raquel')
+      .clear().should('have.value','')
+      cy.get('input[id="lastName"]').should('be.visible').type('Boriero').should('have.value', 'Boriero').clear().should('have.value','')
+      cy.get('input[id="email"]').should('be.visible').type('raquel').should('have.value', 'raquel').clear().should('have.value','')
+      cy.get('input[id="phone"]').should('be.visible').type('123456').should('have.value', '123456').clear().should('have.value','')     
+    })
+
+    //EXERCISE 6 
+    it('displays an error message when submitting the form without filling the required fields', () => {
+    
+      cy.get('button').should('be.visible').click()
+      cy.get('span[class="error"]').should('be.visible') 
+    })
 
 })
