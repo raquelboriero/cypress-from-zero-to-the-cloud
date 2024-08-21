6abbc3955c3e7af2e076f24e654007742d0fd217
Raquel Carvalho <60790005+raquelboriero@users.noreply.github.com>
2024-08-21 19:00:11 +0100

    firstSection

--- cypress/e2e/TAT-CSC.cy.js
+++ cypress/e2e/TAT-CSC.cy.js
@@ -4,103 +4,81 @@ describe('TAT Customer Service Center', () => {
     cy.visit('./src/index.html') // visita a página antes de cada teste
   })
 
-
   it('checks the application title', () => {
     cy.title().should('eq','TAT Customer Service Center')
   })
 
-
-
   //EXERCISE + EXERCISE 1
-  it.only('fills in the required fields and submits the form', () => {
+  it.only('1 - fills in the required fields and submits the form', () => {
     
+    const longText = Cypress._.repeat('abcde', 100)
+
     cy.get('input[id="firstName"]').type('Raquel').should('have.value', 'Raquel')
     cy.get('input[id="lastName"]').type('Boriero').should('have.value', 'Boriero')
     cy.get('input[id="email"]').type('raquel@gmail.com').should('have.value', 'raquel@gmail.com')
     cy.get('textarea[id="open-text-area"]').should('be.visible')
-      .type('O professor Sebastião Salvino, morador de Patrocínio, no Alto Paranaíba, foi o responsável por registrar mais de 200 espécies de pássaros ao longo de 20 anos.', { delay: 0 }) // sem atraso ao digitar o texto
-      //.type('O professor Sebastião Salvino, morador de Patrocínio, no Alto Paranaíba, foi o responsável por registrar mais de 200 espécies de pássaros ao longo de 20 anos.') 
-      .should('have.value', 'O professor Sebastião Salvino, morador de Patrocínio, no Alto Paranaíba, foi o responsável por registrar mais de 200 espécies de pássaros ao longo de 20 anos.')
+      .type(longText, { delay: 0 }) // sem atraso ao digitar o texto
+      .should('have.value', longText)
     cy.contains('button', 'Send').should('be.visible').click()
-    cy.get('span[class="success"]').should('be.visible')   
+    
+    cy.get('.success').should('be.visible')   
   })
 
   //EXERCISE 2 
-  it('displays an error message when submitting the form with an email with invalid formatting', () => {
+  it.only('2 - displays an error message when submitting the form with an email with invalid formatting', () => {
     
-    cy.get('input[id="firstName"]')
-    .should('be.visible')
-    .type('Raquel')
-    .should('have.value', 'Raquel')
-  
-    cy.get('input[id="lastName"]')
-    .should('be.visible')
-    .type('Boriero')
-    .should('have.value', 'Boriero')
-
-    cy.get('input[id="email"]')
-    .should('be.visible')
-    .type('raquel')
-    .should('have.value', 'raquel')
-
-    cy.get('textarea[id="open-text-area"]')
-      .should('be.visible')
-      .type('My text.') 
-      .should('have.value', 'My text.')
-
+    cy.get('#firstName').type('Raquel')
+    cy.get('#lastName').type('Boriero')
+    cy.get('#email').type('raquel').should('have.value', 'raquel')
+    cy.get('#open-text-area').type('My text.').should('have.value', 'My text.')
     cy.contains('button', 'Send').should('be.visible').click()
 
-    cy.get('span[class="error"]')
-    .should('be.visible')   
+    cy.get('.error').should('be.visible')   
   })
 
     //EXERCISE 3 
-  it('validate that if a non-numeric value is entered, its value will remain empty', () => {
+  it.only('3 - validate that if a non-numeric value is entered, its value will remain empty', () => {
     
-    cy.get('input[id="phone"]')
-    .should('be.visible')
-    .type('abc')
-    .should('have.value', '')
+    cy.get('#phone').type('abc').should('have.value', '')
     
   })
 
     //EXERCISE 4 
-  it('displays an error message when the phone becomes required but is not filled in before the form submission', () => {
+  it.only('4 - displays an error message when the phone becomes required but is not filled in before the form submission', () => {
     
-    cy.get('input[id="firstName"]').should('be.visible').type('Raquel').should('have.value', 'Raquel')
-    cy.get('input[id="lastName"]').should('be.visible').type('Boriero').should('have.value', 'Boriero')
-    cy.get('input[id="email"]').should('be.visible').type('raquel').should('have.value', 'raquel')
-
-    cy.get('input[id="phone"]').invoke('attr', 'required-mark', 'required')
-  
+    cy.get('#firstName').type('Raquel')
+    cy.get('#lastName').type('Boriero')
+    cy.get('#email').type('raquel').should('have.value', 'raquel')
+   // cy.get('#phone').invoke('attr', 'required-mark', 'required')
+    cy.get('#phone-checkbox').click()
     cy.contains('button', 'Send').should('be.visible').click()
-    cy.get('span[class="error"]').should('be.visible') 
+  
+    cy.get('.error').should('be.visible') 
   })
 
     //EXERCISE 5 
-    it('fills and clears the first name, last name, email, and phone fields', () => {
+    it.only('5 - fills and clears the first name, last name, email, and phone fields', () => {
     
-      cy.get('input[id="firstName"]').should('be.visible').type('Raquel').should('have.value', 'Raquel').clear().should('have.value','')
-      cy.get('input[id="lastName"]').should('be.visible').type('Boriero').should('have.value', 'Boriero').clear().should('have.value','')
-      cy.get('input[id="email"]').should('be.visible').type('raquel').should('have.value', 'raquel').clear().should('have.value','')
-      cy.get('input[id="phone"]').should('be.visible').type('123456').should('have.value', '123456').clear().should('have.value','')     
+      cy.get('input[id="firstName"]').type('Raquel').should('have.value', 'Raquel').clear().should('have.value','')
+      cy.get('input[id="lastName"]').type('Boriero').should('have.value', 'Boriero').clear().should('have.value','')
+      cy.get('input[id="email"]').type('raquel').should('have.value', 'raquel').clear().should('have.value','')
+      cy.get('input[id="phone"]').type('123456').should('have.value', '123456').clear().should('have.value','')     
     })
 
     //EXERCISE 6 
-    it('displays an error message when submitting the form without filling the required fields', () => {
+    it.only('6 - displays an error message when submitting the form without filling the required fields', () => {
     
       cy.contains('button', 'Send').should('be.visible').click()
-      cy.get('span[class="error"]').should('be.visible') 
+      cy.get('.error').should('be.visible') 
     })
 
 
-    //EXERCISE 7 The test must make use of a fillMandatoryFieldsAndSubmit command, which must be implemented in the cypress/support/commands.js file.
-    // There must be verification that the success message is displayed.
-    it('successfully submits the form using a custom command', () => {
+    //EXERCISE 7 
+    it.only('7 - successfully submits the form using a custom command', () => {
     
-      cy.fillMandatoryFieldsAndSubmit()
-      cy.get('span[class="success"]')
-      .should('be.visible') 
+      cy.fillMandatoryFieldsAndSubmit({ firstName: 'Ricardo', email: 'ricardo@example.com' })
+
+      cy.get('.success').should('be.visible') 
 
     })
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,28 +1,3 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 
 Cypress.Commands.add('fillMandatoryFieldsAndSubmit', (data = {}) => {
@@ -36,12 +11,11 @@ Cypress.Commands.add('fillMandatoryFieldsAndSubmit', (data = {}) => {
     
     const finalData = { ...defaultData, ...data }
 
-    cy.get('input[id="firstName"]').type(finalData.firstName)
-    cy.get('input[id="lastName"]').type(finalData.lastName)
-    cy.get('input[id="email"]').type(finalData.email)
-    cy.get('textarea[id="open-text-area"]').type(finalData.message)
-    cy.get('button[type="submit"]').click()
-    cy.contains('button', 'Send')
+    cy.get('#firstName').type(finalData.firstName)
+    cy.get('#lastName').type(finalData.lastName)
+    cy.get('#email').type(finalData.email)
+    cy.get('#open-text-area').type(finalData.message)
+    cy.contains('button', 'Send').click()
 })
 
 
