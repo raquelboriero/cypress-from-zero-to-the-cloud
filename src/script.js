bcb0e3b4f4a6dd428e67c98fb9ed4296bcc91d55
Raquel Carvalho <60790005+raquelboriero@users.noreply.github.com>
2024-08-27 19:05:27 +0100

    fix broken code

--- src/script.js
+++ src/script.js
@@ -18,7 +18,7 @@ document.querySelector('#phone-checkbox')
 
 document.querySelector('button[type="submit"]')
   .addEventListener('click', function(event) {
-    //event.preventDefault()
+    event.preventDefault()
     const firstNameField = document.getElementById('firstName')
     const lastNameField = document.getElementById('lastName')
     const emailField = document.getElementById('email')
@@ -33,10 +33,10 @@ document.querySelector('button[type="submit"]')
       return showAndHideErrorMessage()
     }
     if (isPhoneRequired && !phoneField.value) {
-   //   return showAndHideErrorMessage()
+     return showAndHideErrorMessage()
     }
     if (!emailField.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
-   //   return showAndHideErrorMessage()
+     return showAndHideErrorMessage()
     }
     firstNameField.value = ''
     lastNameField.value = ''
